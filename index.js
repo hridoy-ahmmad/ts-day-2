98fda500d7c7c10eaf990c5f3767786a4aeb0d7a
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-05-28 03:23:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const userName = 'arif';
+const userAge = 32;
+const userInfo = `${userName} is a good person and he is ${userAge} years old`;
+console.log(userInfo);
+export {};
---
+++ index.ts
@@ -0,0 +1,7 @@
+const userName: string = 'arif';
+const userAge: number = 32
+
+const userInfo = `${userName} is a good person and he is ${userAge} years old`
+console.log(userInfo);
+
+export { }
