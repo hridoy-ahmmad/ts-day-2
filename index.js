5156c9230166daacf6d04ed6b4b22810667785f1
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-05-28 03:30:27 +0600

    number to string

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const userName = 'arif';
 const userAge = 32;
 const userInfo = `${userName} is a good person and he is ${userAge} years old`;
-console.log(userInfo);
+// console.log(userInfo);
+const numberIntoString = userAge.toString();
+console.log(numberIntoString);
 export {};
--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 const userName: string = 'arif';
 const userAge: number = 32
 
-const userInfo = `${userName} is a good person and he is ${userAge} years old`
-console.log(userInfo);
+const userInfo: string | number = `${userName} is a good person and he is ${userAge} years old`
+// console.log(userInfo);
+const numberIntoString: string = userAge.toString()
+console.log(numberIntoString);
+
 
 export { }
